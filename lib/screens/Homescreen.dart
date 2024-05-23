a48f15c981d17dc070335a521868de3dfb30dedf
Yugi <rafaelagoncillo@gmail.com>
2024-05-23 21:28:13 +0800

    reversed

--- lib/screens/Homescreen.dart
+++ lib/screens/Homescreen.dart
@@ -1,34 +1,17 @@
-class Universe extends StatelessWidget {
+import 'package:flutter/material.dart';
+
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Welcome to the Universe'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello, Universe!',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 20),
-            Icon(
-              Icons.star,
-              size: 100,
-              color: Colors.blue,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add your onPressed code here!
-        },
-        tooltip: 'Explore',
-        child: Icon(Icons.explore),
-      ),
-    );
+    return const Placeholder();
   }
 }
+
